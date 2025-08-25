3b1d55bf395931a8def86e731917ecfbca366e10
興怡 <>
2025-08-25 16:22:33 -0700

    client side js

--- src/scripts/main.js
+++
@@ -1,6 +0,0 @@
-import { createApiClientApp } from '@/App'
-
-await createApiClientApp(document.getElementById('scalar-client'), {
-  url: 'https://practice-davinci-838522938854.us-central1.run.app/docs/',
-  proxyUrl: 'https://proxy.scalar.com',
-})
