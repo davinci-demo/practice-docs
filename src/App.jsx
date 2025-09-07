17e95d405727a5fb099b9e4fc4322a28f5bbb7f4
興怡 <>
2025-09-06 20:33:56 -0700

    skip the css that was in the pkg since we are index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import openapiURL from '/openapi.json?url'
 import { ApiReferenceReact } from '@scalar/api-reference-react'
-import '@scalar/api-reference-react/style.css'
 
 function App() {
 
   return (
-    <>
-
     <ApiReferenceReact
       configuration={{
         persistAuth: true,
@@ -22,7 +19,6 @@ function App() {
       }}
     />
 
-    </>
   )
 }
 
